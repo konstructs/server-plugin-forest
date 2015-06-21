8dfdaf65dd21f51e1bc7a0c5aa2be0f16841c448
Petter Arvidsson <petter.arvidsson@gmail.com>
2015-06-21 23:04:39 +0200

    Plugin that extends messages users send

--- src/main/java/org/konstructs/example/MyPlugin.java
+++ src/main/java/org/konstructs/example/MyPlugin.java
@@ -4,23 +4,30 @@ import akka.actor.UntypedActor;
 import akka.actor.Props;
 import konstructs.plugin.Config;
 import konstructs.plugin.PluginConstructor;
-
+import konstructs.api.SayFilter;
+import konstructs.api.Say;
 
 public class MyPlugin extends UntypedActor {
-    private String userMessage;
+    private String responseText;
     private String pluginName;
 
-    public MyPlugin(String pluginName, String userMessage) {
-        userMessage = userMessage;
-        pluginName = pluginName;
+    public MyPlugin(String pluginName, String responseText) {
+        this.responseText = responseText;
+        this.pluginName = pluginName;
     }
 
     public void onReceive(Object message) {
-        unhandled(message);
+        if(message instanceof SayFilter) {
+            SayFilter say = (SayFilter)message;
+            String text = say.message().text();
+            say.continueWith(new Say("Did you just say: " + text + "?"), getSender());
+        } else {
+            unhandled(message);
+        }
     }
 
     @PluginConstructor
-    public static Props props(String pluginName, @Config(key = "user-message") String userMessage) {
-        return Props.create(MyPlugin.class, pluginName, userMessage);
+    public static Props props(String pluginName, @Config(key = "response-text") String responseText) {
+        return Props.create(MyPlugin.class, pluginName, responseText);
     }
 }
